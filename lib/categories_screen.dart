44870aab6643f70b5e59558494f68acdca0c3f7d
mikol <mikolajek.a@gmail.com>
2023-01-06 11:47:59 +0100

    module 164 - passing data via the constructor

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -21,7 +21,8 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.title, catData.color))
+            .map((catData) =>
+                CategoryItem(catData.id, catData.title, catData.color))
             .toList(),
       ),
     );
