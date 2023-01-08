e13a041e66463d7213196615d4a14c37479ab21d
mikol <mikolajek.a@gmail.com>
2023-01-08 12:37:03 +0100

    module 168 - meal module added, meals' titles display by categories form DUMMY_MEALS

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
   // final String categoryId;
   // final String categoryTitle;
   //
@@ -12,13 +15,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: Center(
-        child: Text('The recipes for the category'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,11 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'RobotoCondensed'),
             ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/',
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen(),
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
       },
     );
   }
