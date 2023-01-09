86e519c2bdd06f332bb80b1fc84c7cf23d4e4af6
mikol <mikolajek.a@gmail.com>
2023-01-09 18:54:43 +0100

    module 171 - navigation to the meal detail page

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app_new/models/meal.dart';
+import 'package:meal_app_new/screens/meal_detail_screen.dart';
 
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
@@ -9,7 +11,8 @@ class MealItem extends StatelessWidget {
   final Affordability affordability;
 
   const MealItem(
-      {required this.title,
+      {required this.id,
+      required this.title,
       required this.imageUrl,
       required this.duration,
       required this.complexity,
@@ -47,12 +50,14 @@ class MealItem extends StatelessWidget {
     }
   }
 
-  void selectMeal() {}
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(MealDetailScreen.routeName, arguments: id);
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
